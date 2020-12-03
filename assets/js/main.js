f2a0305a3050e143873aefda246a475b9160f314
angelacaterina <angela.caterina@icloud.com>
2020-12-03 17:53:31 +0100

    btn: prompt
    aggiunto un bottone per permettere all'utente di inserire 3 prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ for(var key in student){
     var studentMarkup =
     `
         <div class="cardStudent">
-          <h2>${firstName} ${lastName} ${age} anni</h2>
+          <h2>${firstName} ${lastName}</h2>
         </div>
     `
   console.log(key, student[key]);
@@ -27,15 +27,18 @@ document.getElementById('student').insertAdjacentHTML('beforeend', studentMarkup
 var studentList = [
   {
     firstName : "Camilla",
-    lastName : "Dia"
+    lastName : "Dia",
+    age : 25
   },
   {
     firstName : "Marco",
-    lastName : "Barazzutti"
+    lastName : "Barazzutti",
+    age : 24
   },
   {
     firstName : "Klaus",
-    lastName : "Brown"
+    lastName : "Brown",
+    age : 34
   }
 ]
 
@@ -44,14 +47,39 @@ console.log(studentList.length, studentList);
 for(var i = 0; i < studentList.length; i++){
   var firstName = studentList[i].firstName;
   var lastName = studentList[i].lastName;
-  var studentMarkup =
+  var age = studentList[i].age;
+  var studentMarkup2 =
   `
-      <div class="cardStudent">
-        <h2>${firstName} ${lastName}</h2>
-      </div>
+    <div class="cardStudent">
+      <h2>${firstName} ${lastName}</h2>
+    </div>
   `
-  console.log(studentList[i].firstName, studentList[i].lastName);
-  document.getElementById('studentList').insertAdjacentHTML('beforeend', studentMarkup);
+  console.log(studentList[i].firstName, studentList[i].lastName, studentList[i].age);
+
+  document.getElementById('studentList').insertAdjacentHTML('beforeend', studentMarkup2);
 }
 
 // 3. Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+$('#btn').click(function(){
+  var newFirstName = prompt("nome");
+  var newLastName = prompt("last name");
+  var newAge = Number(prompt("age"));
+
+  studentList.push({
+    firstName : newFirstName,
+    lastName : newLastName,
+    age : newAge
+  });
+
+  var studentMarkup3 =
+  `
+    <div class="cardStudent">
+      <h2>${newFirstName} ${newLastName}</h2>
+    </div>
+  `
+  console.log(newFirstName, newLastName, newAge);
+  // console.log(studentList[i].firstName, studentList[i].lastName, studentList[i].age); // non mi segna il secondo add student
+
+  document.getElementById('studentList').insertAdjacentHTML('beforeend', studentMarkup3);
+  console.log(studentList);
+})
