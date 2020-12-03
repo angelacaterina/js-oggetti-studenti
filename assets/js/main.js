c9d860893ea9cccf7a2db9165e5eca9f76b9bff3
angelacaterina <angela.caterina@icloud.com>
2020-12-03 17:11:42 +0100

    array di oggetti

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,12 +16,42 @@ for(var key in student){
     var studentMarkup =
     `
         <div class="cardStudent">
-            <h2>${firstName} ${lastName} ${age}</h2>
+          <h2>${firstName} ${lastName} ${age} anni</h2>
         </div>
     `
-    console.log(key, student[key]);
+  console.log(key, student[key]);
 }
 document.getElementById('student').insertAdjacentHTML('beforeend', studentMarkup);
 
 // 2. Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var studentList = [
+  {
+    firstName : "Camilla",
+    lastName : "Dia"
+  },
+  {
+    firstName : "Marco",
+    lastName : "Barazzutti"
+  },
+  {
+    firstName : "Klaus",
+    lastName : "Brown"
+  }
+]
+
+console.log(studentList.length, studentList);
+
+for(var i = 0; i < studentList.length; i++){
+  var firstName = studentList[i].firstName;
+  var lastName = studentList[i].lastName;
+  var studentMarkup =
+  `
+      <div class="cardStudent">
+        <h2>${firstName} ${lastName}</h2>
+      </div>
+  `
+  console.log(studentList[i].firstName, studentList[i].lastName);
+  document.getElementById('studentList').insertAdjacentHTML('beforeend', studentMarkup);
+}
+
 // 3. Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
